471cb84081dda9ca9e1bbc93c989f3a61ef0b425
Phay Someth <phay.someth70@gmail.com>
2025-11-07 09:21:23 +0700

    feat: Add Windows runner and Flutter integration
    - Created a new Windows runner for the Flutter application.
    - Added CMake configuration files for building the Windows application.
    - Implemented the main entry point for the application with command line argument handling.
    - Set up the Flutter window and integrated it with the Windows API.
    - Added resource files including icons and application manifest.
    - Implemented utility functions for console handling and command line argument parsing.
    - Added support for dark mode in the application window.
    - Created necessary header and source files for the Flutter window and Win32 window management.
    - Added a manifest file for DPI awareness and compatibility settings.
    - Included a JSON manifest for web application configuration.

---
+++ lib/main.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("My Hobbies"), centerTitle: false),
+        backgroundColor: Colors.amber,
+        body: Padding(
+          padding: EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              HobbyCard(
+                title: "Travelling",
+                icon: Icons.travel_explore,
+                color: Colors.green,
+              ),
+              SizedBox(height: 10),
+              HobbyCard(
+                title: "Skating",
+                icon: Icons.skateboarding,
+                color: Colors.blueGrey,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HobbyCard extends StatelessWidget {
+  final String title;
+  final IconData icon;
+  final Color? color;
+
+  const HobbyCard({
+    super.key,
+    required this.title,
+    required this.icon,
+    this.color = Colors.blue,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(30),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Icon(icon, color: Colors.white),
+          SizedBox(width: 20),
+          Text(title, style: TextStyle(fontSize: 24, color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
